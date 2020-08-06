403b9a15dd6d04e857ee27cdcd2ac48a3361de88
CodeLikeIce <codelikeice@gmail.com>
2020-08-06 21:41:41 +0000

    ccol

--- forms/auth/SendEmail.php
+++ forms/auth/SendEmail.php
@@ -19,8 +19,8 @@ function sendEmailToClient($name,$email)
         $mail->isSMTP();                                            // Send using SMTP
         $mail->Host       = 'smtp.mailtrap.io';                    // Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-        $mail->Username   = '295af7qq48130bee';                     // SMTP username
-        $mail->Password   = 'fb8c3cf7qq6d118b';                               // SMTP password
+        $mail->Username   = '';                     // SMTP username
+        $mail->Password   = '';                               // SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
         $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
